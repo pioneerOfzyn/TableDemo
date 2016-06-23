1ebbe16e7279ebb60959b0666515289ffafa6f52
pioneer <1833705830@163.com>
2016-06-23 21:23:56 +0800

    添加了一个自定义View

---
+++ HelloWorld/HelloWorld/RandomView.h
@@ -0,0 +1,13 @@
+//
+//  RandomView.h
+//  HelloWorld
+//
+//  Created by pioneer on 16/6/23.
+//  Copyright © 2016年 pioneer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RandomView : UIView
+
+@end
---
+++ HelloWorld/HelloWorld/RandomView.m
@@ -0,0 +1,21 @@
+//
+//  RandomView.m
+//  HelloWorld
+//
+//  Created by pioneer on 16/6/23.
+//  Copyright © 2016年 pioneer. All rights reserved.
+//
+
+#import "RandomView.h"
+
+@implementation RandomView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
