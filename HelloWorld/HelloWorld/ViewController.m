567bbba777bf3b4849e71c46a3316dbe7dad6204
pioneer <1833705830@163.com>
2016-06-23 17:43:27 +0800

    创建表格

--- HelloWorld/HelloWorld/ViewController.m
+++ HelloWorld/HelloWorld/ViewController.m
@@ -7,9 +7,9 @@
 //
 
 #import "ViewController.h"
-
-@interface ViewController ()
-
+#import "UIView+SDAutoLayout.h"
+@interface ViewController ()<UITableViewDataSource>
+@property (strong, nonatomic) UITableView *myTable;
 @end
 
 @implementation ViewController
@@ -17,8 +17,37 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    [self.view addSubview:self.myTable];
 }
+#pragma mark 懒加载
 
+-(UITableView *)myTable
+{
+    if(!_myTable){
+        _myTable = [[UITableView alloc]initWithFrame:CGRectZero style:UITableViewStylePlain];
+        _myTable.tableFooterView = [UIView new];
+        [_myTable registerClass:[UITableViewCell class] forCellReuseIdentifier:NSStringFromClass([UITableViewCell class])];
+        _myTable.dataSource = self;
+        
+    }
+    return _myTable;
+}
+#pragma mark 自动布局
+- (void)viewWillLayoutSubviews
+{
+    _myTable.sd_layout.spaceToSuperView(UIEdgeInsetsZero);
+}
+#pragma mark 协议
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return 10;
+}
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:NSStringFromClass([UITableViewCell class]) forIndexPath:indexPath];
+    cell.textLabel.text = @"Hello world";
+    return cell;
+}
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
